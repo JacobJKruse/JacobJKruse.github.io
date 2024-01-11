dbb5fe98569c8ea2a8376e3463dcf7769ee4ffc5
Jacob Kruse <jakruse02@gmail.com>
2024-01-11 00:33:42 -0600

    skills

--- src/app/components/main/Skills.tsx
+++ src/app/components/main/Skills.tsx
@@ -49,7 +49,7 @@ const Skills = () => {
         
   
         <div className="w-full h-full absolute">
-          <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
+          <div className="w-full h-full z-[-30] opacity-30 absolute flex items-center justify-center bg-cover">
             <video
               className="w-full h-auto"
               preload="false"
