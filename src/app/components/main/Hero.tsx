45e0dfdab7f8e0e24fe75d1862b70fc998b43cee
Jacob Kruse <jakruse02@gmail.com>
2024-01-11 00:25:28 -0600

    Update Hero.tsx

--- src/app/components/main/Hero.tsx
+++ src/app/components/main/Hero.tsx
@@ -4,10 +4,8 @@ import {HeroContent } from "./sub/HeroContent";
 const Hero = () => {
   return (
     <div className=" flex flex-col h-full w-full" id="about-me">
-<video
+<video  loop muted playsInline
   autoPlay
-  muted
-  loop
   className="absolute top-0 h-full w-full left-0 z-[1] object-cover"
   style={{ objectFit: 'cover', transform: 'translateY(-45%) rotate(180deg)' }}
 >
