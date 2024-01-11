521e12b6082dd69d74ad8fba39ea1693739e1bf0
Jacob Kruse <jakruse02@gmail.com>
2024-01-11 00:33:05 -0600

    stuff

--- src/app/components/main/Hero.tsx
+++ src/app/components/main/Hero.tsx
@@ -4,9 +4,12 @@ import {HeroContent } from "./sub/HeroContent";
 const Hero = () => {
   return (
     <div className=" flex flex-col h-full w-full" id="about-me">
-<video  loop muted playsInline
+<video
   autoPlay
-  className="absolute top-0 h-full w-full left-0 z-[1] object-cover"
+  muted
+  loop
+  playsInline
+  className="absolute top-0 h-full w-full left-0 z-[-30] object-cover"
   style={{ objectFit: 'cover', transform: 'translateY(-45%) rotate(180deg)' }}
 >
   <source src="/blackhole.webm" type="video/webm" />
